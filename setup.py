2a1186b5dd8b4a62ae7cc05b2ef01af373e4008b
windzu <windzu1@gamil.com>
2024-05-23 17:34:47 +0800

    update version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def parse_requirements(fname_list=[]):
 setup(
     # 描述信息
     name="apk",
-    version="0.0.6",
+    version="0.0.7",
     description="awesome perception kit",
     author="windzu",
     author_email="windzu1@gmail.com",
